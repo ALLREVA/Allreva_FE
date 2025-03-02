0b31ffefda74e57f1e46a2cccdc8e74e51dc4687
nxnaxx <nxnaxx125@gmail.com>
2025-03-03 01:06:33 +0900

    feat: add empty state to chat list page

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,13 +1,28 @@
 import styled from '@emotion/styled';
 import { useEffect } from 'react';
+import { TbMessageChatbot } from 'react-icons/tb';
 
 import ChatItem from './components/ChatItem';
 
 import { useGetChatList } from 'queries/chat';
 import { useChatStore } from 'stores';
+import { BodyRegularText } from 'styles/Typography';
 
 const ContentContainer = styled.div`
   padding: 1.6rem 0;
+`;
+
+const EmptyChatList = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2.4rem;
+  padding: 24rem 0;
+
+  svg {
+    color: ${({ theme }) => theme.colors.primary};
+  }
 `;
 
 const ChatList = styled.ul``;
@@ -22,7 +37,14 @@ const Chat = () => {
 
   return (
     <ContentContainer>
-      <ChatList>{chatRooms?.map((data) => <ChatItem key={data.chatId} {...data} />)}</ChatList>
+      {chatRooms.length === 0 ? (
+        <EmptyChatList>
+          <TbMessageChatbot size={64} />
+          <BodyRegularText>참여 중인 채팅이 없어요</BodyRegularText>
+        </EmptyChatList>
+      ) : (
+        <ChatList>{chatRooms?.map((data) => <ChatItem key={data.chatId} {...data} />)}</ChatList>
+      )}
     </ContentContainer>
   );
 };
